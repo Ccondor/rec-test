6620572bea76b71050d3241874b341a2886a398d
Carlos <carlos@hashdog.com>
2019-08-15 19:07:07 -0300

    New Test Cases

--- Test Suites/LoginWrongPassword.ts
+++ Test Suites/LoginWrongPassword.ts
@@ -1,7 +1,7 @@
 <?xml version="1.0" encoding="UTF-8"?>
 <TestSuiteEntity>
    <description></description>
-   <name>Login</name>
+   <name>LoginWrongPassword</name>
    <tag></tag>
    <isRerun>false</isRerun>
    <mailRecipient></mailRecipient>
@@ -9,11 +9,5 @@
    <pageLoadTimeout>30</pageLoadTimeout>
    <pageLoadTimeoutDefault>true</pageLoadTimeoutDefault>
    <rerunFailedTestCasesOnly>false</rerunFailedTestCasesOnly>
-   <testSuiteGuid>d3ecad6a-786d-48bc-ae8a-049071c118cc</testSuiteGuid>
-   <testCaseLink>
-      <guid>26d9c9fa-3bb5-43f9-b539-fd7f80899b3c</guid>
-      <isReuseDriver>false</isReuseDriver>
-      <isRun>true</isRun>
-      <testCaseId>Test Cases/Login/loginCorrect</testCaseId>
-   </testCaseLink>
+   <testSuiteGuid>1cc37bc1-31f9-4454-b37b-ac541850e093</testSuiteGuid>
 </TestSuiteEntity>
