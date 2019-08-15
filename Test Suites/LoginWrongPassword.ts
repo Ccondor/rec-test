0a53d425247cbb407f60794e4f68864acc479b5d
Carlos <carlos@hashdog.com>
2019-08-15 19:12:35 -0300

    New Test Suites

--- Test Suites/LoginWrongPassword.ts
+++ Test Suites/LoginWrongPassword.ts
@@ -10,4 +10,10 @@
    <pageLoadTimeoutDefault>true</pageLoadTimeoutDefault>
    <rerunFailedTestCasesOnly>false</rerunFailedTestCasesOnly>
    <testSuiteGuid>1cc37bc1-31f9-4454-b37b-ac541850e093</testSuiteGuid>
+   <testCaseLink>
+      <guid>2f7b8513-e498-4519-bb04-66952f3f4e11</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/Login/loginWrongPassword</testCaseId>
+   </testCaseLink>
 </TestSuiteEntity>
---
+++ Test Suites/REC.ts
@@ -0,0 +1,31 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<TestSuiteEntity>
+   <description></description>
+   <name>REC</name>
+   <tag></tag>
+   <isRerun>false</isRerun>
+   <mailRecipient></mailRecipient>
+   <numberOfRerun>0</numberOfRerun>
+   <pageLoadTimeout>30</pageLoadTimeout>
+   <pageLoadTimeoutDefault>true</pageLoadTimeoutDefault>
+   <rerunFailedTestCasesOnly>false</rerunFailedTestCasesOnly>
+   <testSuiteGuid>310221e4-da92-4562-a076-3692068ac105</testSuiteGuid>
+   <testCaseLink>
+      <guid>16497c83-0e1c-43d2-bb9b-511610b14aa1</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/Login/loginCorrect</testCaseId>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>cb3810bc-bfff-4984-9606-1fcb45bb01c4</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/Login/loginWrongPassword</testCaseId>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>85cb2462-3fc4-48cf-8c05-a66e93ac4144</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/Login/loginWrongUsername</testCaseId>
+   </testCaseLink>
+</TestSuiteEntity>
